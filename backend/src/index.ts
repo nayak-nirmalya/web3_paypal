3d4b718e8597eed983dbda06b6819d96dabb5a2c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-11 11:16:58 +0530

    added get api to getName

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import Moralis from "moralis";
 import cors from "cors";
 import "dotenv/config";
+import ABI from "./abi.json";
 
 const app = express();
 
@@ -9,7 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/getNameAndBalance", async (req, res) => {
-  return res.status(200).json({});
+  const { userAddress } = req.query;
+
+  const response = await Moralis.EvmApi.utils.runContractFunction({
+    chain: process.env.CHAIN_ID,
+    address: process.env.CONTRACT_ADDRESS,
+    functionName: "getMyName",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseName = response.raw;
+
+  const jsonResponse = { name: jsonResponseName };
+
+  return res.status(200).json(jsonResponse);
 });
 
 Moralis.start({
