2e459eb0fc684d97ebadc25f8337fa97f12ed5d9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-11 11:55:19 +0530

    added get api to get histor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function convertArrayToObjects(arr: []) {
+  console.log(arr);
+
+  if (arr.length === 0)
+    return [
+      {
+        key: "",
+        type: "",
+        amount: "",
+        message: "",
+        address: "",
+        subject: "",
+      },
+    ];
+
+  const dataArray = arr.map((transaction, index) => ({
+    key: (arr.length + 1 - index).toString(),
+    type: transaction[0],
+    amount: transaction[1],
+    message: transaction[2],
+    address: `${(transaction[3] as string).slice(0, 4)}...${(
+      transaction[3] as string
+    ).slice(38)}`,
+    subject: transaction[4],
+  }));
+
+  return dataArray.reverse();
+}
+
 app.get("/getNameAndBalance", async (req, res) => {
   const { userAddress } = req.query;
 
@@ -39,10 +68,23 @@ app.get("/getNameAndBalance", async (req, res) => {
     tokenRes.raw.usdPrice * Number(jsonResponseBal)
   ).toFixed(2);
 
+  const historyRes = await Moralis.EvmApi.utils.runContractFunction({
+    chain: process.env.CHAIN_ID,
+    address: process.env.CONTRACT_ADDRESS,
+    functionName: "getMyHistory",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseHistory = convertArrayToObjects(
+    historyRes.raw as unknown as []
+  );
+
   const jsonResponse = {
     name: jsonResponseName,
     balance: jsonResponseBal,
     dollars: jsonResponseDollar,
+    history: jsonResponseHistory,
   };
 
   return res.status(200).json(jsonResponse);
