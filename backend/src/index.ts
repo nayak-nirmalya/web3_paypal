20b81bcfcba10090cdf162b933404a6c8d82e02d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-11 10:41:31 +0530

    added PayPal contract ABI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/getNameAndBalance", async (req, res) => {
+  return res.status(200).json({});
+});
+
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
