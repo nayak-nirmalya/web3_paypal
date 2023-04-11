b83738242e2f0c7d2af0b1a0f4cf3c435ad73012
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-11 10:37:06 +0530

    added types package to dev dep

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,3 +4,14 @@ import cors from "cors";
 import "dotenv/config";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+Moralis.start({
+  apiKey: process.env.MORALIS_KEY,
+}).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on PORT: ${process.env.PORT} for API Calls.`);
+  });
+});
